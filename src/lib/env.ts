a0f1f0f1ac3752c2a1dd8cb09d098f901fb201c3
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-09-23 22:53:06 +0330

    feat: Set up t3 env for typesafe environment variables

---
+++ src/lib/env.ts
@@ -0,0 +1,8 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+
+export const env = createEnv({
+  server: {},
+  client: {},
+  experimental__runtimeEnv: {},
+  isServer: typeof window === "undefined",
+});
