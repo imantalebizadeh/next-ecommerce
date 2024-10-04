85ec3c1252a7cb43ffcd966449e6e1e94793f98c
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-10-04 10:29:01 +0330

    feat: Change the schema and out paths

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
+import { loadEnvConfig } from "@next/env";
 import { defineConfig } from "drizzle-kit";
 
 import { env } from "@/lib/env";
 
+loadEnvConfig(process.cwd());
+
 export default defineConfig({
-  schema: "./src/drizzle/schema/*",
-  out: "./src/drizzle/migrations",
+  schema: "./src/server/db/schema/*.ts",
+  out: "./src/server/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
