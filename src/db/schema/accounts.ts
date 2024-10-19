0cba7337fb8dc94ef199cf8e23615d6b8e6e4333
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-10-19 22:55:57 +0330

    feat: Create auth models

---
+++ src/db/schema/accounts.ts
@@ -0,0 +1,28 @@
+import { integer, pgTable, primaryKey, text } from "drizzle-orm/pg-core";
+import type { AdapterAccountType } from "next-auth/adapters";
+
+import { users } from "./users";
+
+export const accounts = pgTable(
+  "accounts",
+  {
+    userId: text()
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text().$type<AdapterAccountType>().notNull(),
+    provider: text().notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text(),
+    access_token: text(),
+    expires_at: integer(),
+    token_type: text(),
+    scope: text(),
+    id_token: text(),
+    session_state: text(),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  }),
+);
